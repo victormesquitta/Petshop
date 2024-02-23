407235cc974a82a3b583d20aa3b9082a9c76bc8c
Victor Mesquitta <mvictormesquita@gmail.com>
2024-02-23 18:24:00 -0300

    subindo arquivos spring

---
+++ src/test/java/br/senac/tads/petshop/PetshopApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senac.tads.petshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
