99b59f88a0e199e59e1b13523dd62f3246aa83d6
Victor Mesquitta <mvictormesquita@gmail.com>
2024-03-10 02:57:08 -0300

    atualizando scripts

---
+++ InsereEnderecos.sql
@@ -0,0 +1,17 @@
+INSERT INTO t_endereco (cep, endereco, numero, complemento, bairro, cidade, uf, pontoref, tipoLocal, codcliente)
+VALUES ('12345-678', 'Rua das Flores', 123, 'Apto 101', 'Centro', 'São Paulo', 'SP', 'Próximo ao mercado', 'Casa', 1);
+
+INSERT INTO t_endereco (cep, endereco, numero, complemento, bairro, cidade, uf, pontoref, tipoLocal, codcliente)
+VALUES ('54321-876', 'Avenida das Palmeiras', 456, 'Sala 201', 'Jardins', 'Rio de Janeiro', 'RJ', 'Próximo ao parque', 'Trabalho', 2);
+
+INSERT INTO t_endereco (cep, endereco, numero, complemento, bairro, cidade, uf, pontoref, tipoLocal, codcliente)
+VALUES ('98765-432', 'Rua dos Girassóis', 789, NULL, 'Vila Nova', 'Belo Horizonte', 'MG', NULL, 'Outro local', 3);
+
+INSERT INTO t_endereco (cep, endereco, numero, complemento, bairro, cidade, uf, pontoref, tipoLocal, codcliente)
+VALUES ('01234-567', 'Avenida dos Anjos', 789, 'Apto 301', 'Anjolandia', 'Brasília', 'DF', 'Próximo à praça', 'Casa', 4);
+
+INSERT INTO t_endereco (cep, endereco, numero, complemento, bairro, cidade, uf, pontoref, tipoLocal, codcliente)
+VALUES ('45678-901', 'Rua das Estrelas', 101, NULL, 'Estrelado', 'Porto Alegre', 'RS', 'Próximo ao cinema', 'Trabalho', 5);
+
+INSERT INTO t_endereco (cep, endereco, numero, complemento, bairro, cidade, uf, pontoref, tipoLocal, codcliente)
+VALUES ('89012-345', 'Travessa das Rosas', 555, 'Sala 10', 'Jardim das Flores', 'Florianópolis', 'SC', NULL, 'Outro local', 6);
--- SelecionaTabelas.sql
+++ SelecionaTabelas.sql
@@ -1,1 +1,5 @@
 select * from t_cliente;
+select * from t_endereco;
+SHOW FULL COLUMNS FROM t_endereco;
+
+SHOW DATABASES;
