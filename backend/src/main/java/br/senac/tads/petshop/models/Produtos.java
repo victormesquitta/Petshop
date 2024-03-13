47bad43f59b212c853808a34586628df35c915a9
JoaoFarhat <joaofarhat9@gmail.com>
2024-03-13 20:53:11 -0300

    o que tinha na maquina

---
+++ backend/src/main/java/br/senac/tads/petshop/models/Produtos.java
@@ -0,0 +1,64 @@
+package br.senac.tads.petshop.models;
+
+import br.senac.tads.petshop.dtos.EnderecoDTO;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "t_produtos")
+public class Produtos{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "codprodutos")
+    private Integer codprodutos;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "descricao")
+    private String descricao;
+
+    @Column(name = "preco")
+    private double preco;
+
+    @Column(name = "qtd_estoque")
+    private int qtd_estoque;
+
+    /* por hora string, depois id de outra categoria */
+    @Column(name = "categoria")
+    private String categoria;
+
+    @Column (name = "marca")
+    private String marca;
+
+    @Column (name = "imagem")
+    private byte[] imagem;
+
+    @Column (name = "disponivel")
+    private boolean disponivel;
+
+    @Column (name = "dt_criacao")
+    private DateTime dt_criacao;
+
+    @Column (name = "dt_modificacao")
+    private DateTime dt_modificacao;
+
+    @Column (name = "destaque")
+    private boolean destaque;
+
+    @Column (name = "promocao")
+    private boolean promocao;
+
+    @Column (name = "avaliacao")
+    private double avaliacao;
+
+    @Column (name = "nm_avaliacao")
+    private int nm_avaliacao;
+}
