69863bd2e06f19eef5675b8734dafc940e6974fe
JoaoFarhat <joaofarhat9@gmail.com>
2024-04-03 16:37:41 -0300

    remoção de imports aleatorios que nao seriam usados

--- backend/src/main/java/br/senac/tads/petshop/mappers/FuncionarioDTOMapper.java
+++ backend/src/main/java/br/senac/tads/petshop/mappers/FuncionarioDTOMapper.java
@@ -1,34 +1,34 @@
 package br.senac.tads.petshop.mappers;
 
-import br.senac.tads.petshop.dtos.FuncionariosDTO;
-import br.senac.tads.petshop.models.Funcionarios;
+import br.senac.tads.petshop.dtos.FuncionarioDTO;
+import br.senac.tads.petshop.models.Funcionario;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class FuncionariosDTOMapper {
+public class FuncionarioDTOMapper {
     private final ModelMapper modelMapper;
 
     @Autowired
-    public FuncionariosDTOMapper(ModelMapper modelMapper) {
+    public FuncionarioDTOMapper(ModelMapper modelMapper) {
         this.modelMapper = modelMapper;
     }
     // usado para post -> não precisa de id porque ainda não foi criado
-    public Funcionarios toEntity(FuncionariosDTO funcionariosDTO) {
-        Funcionarios funcionarios = modelMapper.map(funcionariosDTO, Funcionarios.class);
+    public Funcionario toEntity(FuncionarioDTO funcionariosDTO) {
+        Funcionario funcionarios = modelMapper.map(funcionariosDTO, Funcionario.class);
         return funcionarios;
     }
 
     // usado para put -> o id foi criado e deve ser mantido
-    public Funcionarios toEntity(FuncionariosDTO FuncionariosDTO, Integer id) {
-        Funcionarios funcionarios = modelMapper.map(FuncionariosDTO, Funcionarios.class);
+    public Funcionario toEntity(FuncionarioDTO FuncionariosDTO, Integer id) {
+        Funcionario funcionarios = modelMapper.map(FuncionariosDTO, Funcionario.class);
         funcionarios.setCodFuncionario(id);
         return funcionarios;
     }
 
-    public FuncionariosDTO toDTO(Funcionarios Funcionarios) {
-        return modelMapper.map(Funcionarios, FuncionariosDTO.class);
+    public FuncionarioDTO toDTO(Funcionario Funcionarios) {
+        return modelMapper.map(Funcionarios, FuncionarioDTO.class);
     }
 
 }
