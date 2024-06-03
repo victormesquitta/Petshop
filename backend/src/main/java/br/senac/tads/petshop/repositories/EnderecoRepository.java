e9f7f57d1312395a2c1198b35de44cde8e3d41d2
JoaoFarhat <joaofarhat9@gmail.com>
2024-06-03 20:29:34 -0300

    permitindo todos criar produtos (por enquanto)

--- backend/src/main/java/br/senac/tads/petshop/repositories/EnderecoRepository.java
+++ backend/src/main/java/br/senac/tads/petshop/repositories/EnderecoRepository.java
@@ -1,8 +1,11 @@
 package br.senac.tads.petshop.repositories;
 
 import br.senac.tads.petshop.models.Endereco;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
-
+    List<Endereco> findByClienteId(Integer clienteId);
 }
